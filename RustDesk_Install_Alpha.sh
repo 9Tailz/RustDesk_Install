cb104e54929776b740ec439fe0a3be2c55823ce7
9Tailz <jordo-t@hotmail.com>
2025-11-27 18:32:17 +1300

    Check Pull and Install_ALpha

--- RustDesk_Install_Alpha.sh
+++ RustDesk_Install_Alpha.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 os=$(uname -s)
 
@@ -7,13 +8,20 @@ case $os in
         echo "OS is Linux"
         if grep -q "debian" /etc/os-release; then
             echo "OS is Debian"
-            url=$(curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest | jq -r '.assets[] | select(.name | contains("x86_64.deb")) | .browser_download_url')
+            url=$(curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest | grep -o '"browser_download_url": *"[^"]*\x86_64.deb"' | sed 's/"browser_download_url": "//;s/"//g')
             echo $url
+            packagename=${url##*/}
+            echo "Grabing Latest Version..."
+            wget $url
+            echo "Installing..."
+            sudo apt install -fy ./$packagename
+
         
         elif grep -q "arch" /etc/os-release; then
             echo "OS is Arch"
-            url=$(curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest | jq -r '.assets[] | select(.name | contains("x86_64.pkg.tar.zst")) | .browser_download_url')
+            url=$(curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest | grep -o '"browser_download_url": *"[^"]*\x86_64.pkg.tar.zst"' | sed 's/"browser_download_url": "//;s/"//g')
             echo $url
+            packagename=${url##*/}
 
         else
             echo "Not Valid Linux OS" 
@@ -23,7 +31,7 @@ case $os in
     
     Darwin)
         echo "OS is Mac"
-        url=$(curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest | jq -r '.assets[] | select(.name | contains("x86_64.dmg")) | .browser_download_url')
+        url=$(curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest | grep -o '"browser_download_url": *"[^"]*\x86_64.dmg"' | sed 's/"browser_download_url": "//;s/"//g')
         echo $url
         ;;
     
