28f5720fb6b39d488f26d176a912562f03bbd868
9 Tailz <jordo-t@Hotmail.com>
2025-12-17 13:18:24 +1300

    more clean up

--- encrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# openssl enc -aes-256-cbc -e -in text.txt -out secret.env -base64 -pbkdf2 -pass "pass: password"
-
-openssl enc -aes-256-cbc -e -in paswd.txt -out secret2.enc -base64 -pbkdf2 -pass "pass: asdasd1032"
